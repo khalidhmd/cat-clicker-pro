e62fa48f9dc31669d4b2178c554c72a28b0843d4
khalidhmd <khalidhmd@outlook.com>
2018-06-23 23:23:47 +0200

    add admin form functionality

--- js/app.js
+++ js/app.js
@@ -5,30 +5,35 @@ var model = {
     currentCat: null,
     cats: [
         {
+            catIndex:0,
             clickCount : 0,
             name : 'Tabby',
             imgSrc : 'img/434164568_fea0ad4013_z.jpg',
             imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
         },
         {
+            catIndex: 1,
             clickCount : 0,
             name : 'Tiger',
             imgSrc : 'img/4154543904_6e2428c421_z.jpg',
             imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904'
         },
         {
+            catIndex: 2,
             clickCount : 0,
             name : 'Scaredy',
             imgSrc : 'img/22252709_010df3379e_z.jpg',
             imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
         },
         {
+            catIndex: 3,
             clickCount : 0,
             name : 'Shadow',
             imgSrc : 'img/1413379559_412a540d29_z.jpg',
             imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
         },
         {
+            catIndex: 4,
             clickCount : 0,
             name : 'Sleepy',
             imgSrc : 'img/9648464288_2516b35537_z.jpg',
@@ -49,6 +54,7 @@ var octopus = {
         // tell our views to initialize
         catListView.init();
         catView.init();
+        adminFormView.init();
     },
 
     getCurrentCat: function() {
@@ -62,12 +68,19 @@ var octopus = {
     // set the currently-selected cat to the object passed in
     setCurrentCat: function(cat) {
         model.currentCat = cat;
+        adminFormView.render();
     },
 
     // increments the counter for the currently-selected cat
     incrementCounter: function() {
         model.currentCat.clickCount++;
         catView.render();
+        adminFormView.render();
+    },
+
+    updateCat: function () {
+        catView.render();
+        catListView.render();
     }
 };
 
@@ -144,5 +157,54 @@ var catListView = {
     }
 };
 
+var adminFormView = {
+    init: function () {
+        // store pointers to our DOM elements for easy access later
+        this.formDisplay = false;
+        this.adminButtonElem = document.getElementById('adminbutton');
+        this.adminFormElem = document.getElementById('adminform');
+        this.catNameElem = document.getElementById('catnametextid');
+        this.catCountElem = document.getElementById('catclickstextid');
+        this.catURLElem = document.getElementById('caturltextid');
+        this.saveButtonElem = document.getElementById('savebutton');
+        this.cancelButtonElem = document.getElementById('cancelbutton');
+
+        this.adminFormElem.style.display = 'none';
+        var self = this;
+        // on click, display admin form
+        this.adminButtonElem.addEventListener('click', function () {
+            self.formDisplay = true;  
+            self.render();
+        });
+
+        this.cancelButtonElem.addEventListener('click', function () {
+            self.formDisplay = false;
+            self.adminFormElem.style.display = 'none';
+        });
+
+        this.saveButtonElem.addEventListener('click', function (e) {
+            self.formDisplay = false;
+            var cat = octopus.getCurrentCat();
+
+            cat.clickCount = self.catCountElem.value
+            cat.name = self.catNameElem.value
+            cat.imgAttribution = self.catURLElem.value
+            model[cat.catIndex] = cat;
+            octopus.updateCat();
+            self.adminFormElem.style.display = 'none';
+            e.preventDefault();
+        });
+
+    },
+
+    render: function () {
+        // update the DOM elements with values from the current cat
+        if (this.formDisplay == true) this.adminFormElem.style.display = 'block';
+        var currentCat = octopus.getCurrentCat();
+        this.catCountElem.value = currentCat.clickCount;
+        this.catNameElem.value = currentCat.name;
+        this.catURLElem.value = currentCat.imgAttribution;
+    }
+};
 // make it go!
 octopus.init();
